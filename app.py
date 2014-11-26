906efcaca5c0c5f5ddf25b9abb3505bd7063f2f1
Spike Frye <sebfrye@gmail.com>
2014-11-25 21:01:35 -0600

    Converted template over to Jade.

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 
 app.debug = True
 
+# Add jade template support
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
 @app.route("/")
 def hello():
     restaurant = Restaurant.objects.first()
 
-    return render_template('home.html', restaurant=restaurant)
+    return render_template('home.jade', restaurant=restaurant)
 
 if __name__ == "__main__":
     app.run()
