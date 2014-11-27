1a91335d7150a6bab7bd26fa666407a10124e13f
Spike Frye <sebfrye@gmail.com>
2014-11-26 20:50:36 -0600

    Created restaurant detail page. Abstracted out base template.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, redirect, url_for
 from mongoengine import connect
+from mongoengine.errors import ValidationError
 
 from documents import Restaurant
 
@@ -12,10 +13,22 @@ app.debug = True
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route("/")
-def hello():
-    restaurant = Restaurant.random
-    
-    return render_template('home.jade', restaurant=restaurant)
+def random_restaurant():
+	restaurant = Restaurant.random
+
+	return render_template('home.jade', restaurant=restaurant)
+
+@app.route("/restaurant/<restaurant_id>")
+def restaurant_details(restaurant_id):
+	try:
+		restaurant = Restaurant.objects.filter(id=restaurant_id).first()
+	except ValidationError:
+		restaurant = None
+
+	if restaurant is None:
+		abort(404)
+
+	return render_template('restaurant/details.jade', restaurant=restaurant)
 
 if __name__ == "__main__":
     app.run()
