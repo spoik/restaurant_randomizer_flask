b6d715823ca243b03436032b874111cdd4460e04
Spike Frye <sebfrye@gmail.com>
2014-11-28 17:09:01 -0600

    Created endpoint that returns all the restaurant data.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, abort, redirect, request, url_for
+from flask import abort, Flask, redirect, render_template, Response, request, url_for
 
 from mongoengine.errors import ValidationError
 
 from db import db
 from documents import Restaurant
+
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -13,27 +14,38 @@ app.config['MONGODB_SETTINGS'] = {
 app.debug = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+# Initialize flask-mongoengine
 db.init_app(app)
 
 # Add jade template support
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
+
 @app.route("/")
 def random_restaurant():
+	"""
+	Shows a random restaurant to the user
+	"""
 	restaurant = Restaurant.random
 
 	return render_template('home.jade', restaurant=restaurant)
 
-# Restaurant details page
+
 @app.route("/restaurant/<restaurant_id>")
 def restaurant_details(restaurant_id):
+	"""
+	Restaurant details page
+	"""
 	restaurant = Restaurant.objects.get_or_404(id=restaurant_id)
 
 	return render_template('restaurant/details.jade', restaurant=restaurant)
 
-# Restaurant create page
+
 @app.route("/restaurant/create/", methods=['GET', 'POST'])
 def restaurant_create():
+	"""
+	Restaurant create page
+	"""
 	from document_forms import RestaurantForm
 
 	form = RestaurantForm(request.form)
@@ -47,6 +59,17 @@ def restaurant_create():
 		form = RestaurantForm()
 
 	return render_template('restaurant/create.jade', form=form, message=message)
+
+
+@app.route("/api/restaurants/")
+def api_restaurants():
+	"""
+	Returns a JSON object with all the restaurant data.
+	"""
+	response = Response(Restaurant.objects.to_json(),
+		status=200, mimetype="application/json")
+
+	return response
 
 
 if __name__ == "__main__":
--- documents.py
+++ documents.py
@@ -8,8 +8,15 @@ from db import db
 class Restaurant(db.Document):
     name = fields.StringField(required=True, max_length=200)
 
+    meta = {
+    	"ordering": ["name"]
+    }
+
     @queryset_manager
     def random(self, queryset):
+    	"""
+    	Returns a random record from the collection.
+    	"""
         num_entries = queryset.count()
         random_entry_index = random.randint(0, num_entries-1)
         return queryset[random_entry_index]
