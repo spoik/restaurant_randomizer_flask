7d0cb92f5b57be3c0faef1dc7e0508958201e268
Spike Frye <sebfrye@gmail.com>
2014-11-28 18:01:38 -0600

    Handle no restaurants in db better. It just crashed before.

--- documents.py
+++ documents.py
@@ -18,5 +18,11 @@ class Restaurant(db.Document):
     	Returns a random record from the collection.
     	"""
         num_entries = queryset.count()
+
+        # If there are no entries in the collection, return None.
+        # This is consitant with what first() returns when the collection is empty.
+        if num_entries == 0:
+        	return None
+
         random_entry_index = random.randint(0, num_entries-1)
         return queryset[random_entry_index]
